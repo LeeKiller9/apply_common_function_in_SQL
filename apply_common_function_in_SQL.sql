fb7eec33f2016bd998e2c75f2a602a8d2ac8a38d
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-11-21 11:22:50 +0700

    up files

---
+++ apply_common_function_in_SQL.sql
@@ -0,0 +1,15 @@
+use quanlysinhvien2;
+
+SELECT sub.SubID, sub.SubName, sub.Credit from `subject` sub
+where sub.Credit >= all (select Credit from `subject`);
+
+-- select * from `subject`
+-- where Credit = max(Credit);
+
+select * from mark_table mtb
+where mtb.Mark >= all (select mark_table.Mark from mark_table);
+
+select student.*, avg(mark_table.Mark) as `AVG`
+from student join mark_table on student.StudentId = mark_table.StudentId
+group by mark_table.StudentId
+order by `AVG` DESC;
